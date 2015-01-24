b3af5e699192cdc78d924f40370cfa81e513b963
Wu Wenqi <Wenqi@Wenqis-MacBook-Pro.local>
2015-01-24 15:05:43 +0800

    Initial Commit

---
+++ Tackboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tackboard
+//
+//  Created by Wu Wenqi on 24/1/15.
+//  Copyright (c) 2015 Wenqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
